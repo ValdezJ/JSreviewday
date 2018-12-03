b63c8a7227100724631a7d12813a584ebb32d630
learn <learn@learn14.local>
2018-12-03 13:18:12 -0800

    Includes Multi Array

--- duplicates.js
+++ duplicates.js
@@ -1,21 +1,44 @@
 // Create a function that takes in two arrays of numbers and checks for duplicaters.
 // The function should output a new array without duplicaters
 
-var a = [1,2,3,30,17]
-var b = [2,30,1,15]
-// expected output [1,2,3,17,15,30]
-function removeDuplicates(arr1, arr2){
-  newArr = []
-//compare arr1 element with arr2
-//if element of arr2 is not found in arr1, then push arr2 element
-//looping arr2
-  arr1.forEach(function(value){
-    if (arr2.includes(value)) {
-    }
-    else{newArr.push(value)
-    }
-  })
-var cleanArray = newArr.concat(arr2)
-  return cleanArray
-}
-removeDuplicates(a,b)
+// var a = [1,2,3,30,17]
+// var b = [2,30,1,15]
+// // expected output [1,2,3,17,15,30]
+// function removeDuplicates(arr1, arr2){
+//   newArr = []
+// //compare arr1 element with arr2
+// //if element of arr2 is not found in arr1, then push arr2 element
+// //looping arr2
+//   arr1.forEach(function(value){
+//     if (arr2.includes(value)) {
+//     }
+//     else{newArr.push(value)
+//     }
+//   })
+// var cleanArray = newArr.concat(arr2)
+//   return cleanArray
+// }
+// removeDuplicates(a,b)
+
+
+var a = [1, 2, 3, 30, 17];
+var b = [2, 30, 1, 15, ];
+var c = [2, 10, 19, 1, 3]
+var d = [1, 3, 40, 32, 3]
+
+
+function removeDuplicatesMulti(arr1, arr2, arr3, arr4){
+  var combinedArr = arr1.concat(arr2, arr3, arr4)
+  var finalArray = []
+    combinedArr.forEach(function(value){
+      if (finalArray.includes(value)){
+    } else {
+      finalArray.push(value)
+      }
+    })
+      return finalArray
+  }
+
+
+removeDuplicatesMulti(a,b,c,d)
+console.log(removeDuplicatesMulti(a,b,c,d));
--- palindrome.js
+++ palindrome.js
@@ -1,17 +1,44 @@
+// // Create a function that takes in a word and returns whether or not the word s a palindrome (same forwards and backwards)
+// //Function to take 1 argument
+//   //function palindrome(word)
+// //Variable to have "string" in reverse
+// //Method to reverse the "string"
+// // if code - to return whether or not a "string" is a palindrome
+//
+// var testWord = "pod"
+// function palindrome(word){
+//   var reverseWord = word.split("").reverse().join("");
+//   if (word===reverseWord){
+//     return word + " is a palindrome"
+//   } else if (word!==reverseWord){
+//     return word + " is not a palindrome"
+//   }
+// }
+// console.log(palindrome(testWord));
+//
+
+
+var testWord = "dAmmit im mad"
+//Create function to remove spaces & convert Capital Letters to lower case
+// function tidyUp(word){
+//
+//   return tidiedWord;
+// }
 // Create a function that takes in a word and returns whether or not the word s a palindrome (same forwards and backwards)
 //Function to take 1 argument
-  //function palindrome(word)
-//Variable to have "string" in reverse
-//Method to reverse the "string"
-// if code - to return whether or not a "string" is a palindrome
-
-var testWord = "pod"
 function palindrome(word){
-  var reverseWord = word.split("").reverse().join("");
-  if (word===reverseWord){
+  var whiteSpaceRegex = /\s/g
+  var tidiedWord = word.toLowerCase().replace(whiteSpaceRegex,'')
+  //Variable to have "string" in reverse
+  //Method to reverse the "string"
+  var reverseWord = tidiedWord.split("").reverse().join("");
+  // if code - to return whether or not a "string" is a palindrome
+  if (reverseWord===tidiedWord){
     return word + " is a palindrome"
-  } else if (word!==reverseWord){
+  } else if (reverseWord!==tidiedWord){
     return word + " is not a palindrome"
   }
 }
+
+// tidyUp(testWord)
 console.log(palindrome(testWord));
